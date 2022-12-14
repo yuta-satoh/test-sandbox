480b12bb952ba264becbba3fdd24fa0230179368
yuta-satoh <107334898+yuta-satoh@users.noreply.github.com>
2022-12-14 22:22:16 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,63 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
+
+// const test = 5;
+
+// console.log(test);
+
+// const myProfile = {
+//    name:"test",
+//    age : 5
+// };
+
+// console.log(`私の名前は${myProfile.name}です。年齢は${myProfile.age}歳です`);
+
+// const {name,age}=myProfile;
+
+// console.log(`私の名前は${name}です。年齢は${age}歳です`);
+
+// const myProfile = ["test",28];
+
+// const massage = `私の名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です`;
+
+// console.log(massage);
+
+// const [name,age]=myProfile;
+
+// console.log(`私の名前は${name}です。年齢は${age}歳です`);
+
+// const sayHallo =(name = "ゲスト")=> console.log(`こんにちは${name}さん`);
+// sayHallo("aaa");
+
+// const test =[1,2,3,4,5,6,7,8,9];
+
+// // console.log(test);
+
+// // console.log(...test);
+
+// const sum=(num1,num2)=>console.log(num1+num2);
+// sum(test[0],test[1]);
+// sum(...test);
+
+// const test=[1,2,3,4,5];
+// const [num1,num2,...test2]=test;
+// console.log(num1);
+// console.log(num2);
+// console.log(test2);
+
+// const test=[10,20];
+// const test2=[30,40];
+
+// const test3=[...test2];
+// console.log(test3);
+
+// const test4 = [...test,...test2];
+// console.log(test4);
